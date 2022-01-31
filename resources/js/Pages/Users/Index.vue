749b5c20fe308bab9559ccc67526312cf22e7010
Marcis Zoldners <marcis.zoldners@gmail.com>
2022-02-01 00:19:25 +0200

    Inertia Forms 101

--- resources/js/Pages/Users/Index.vue
+++ resources/js/Pages/Users/Index.vue
@@ -42,7 +42,7 @@
 </template>
 
 <script setup>
-import Pagination from "../Shared/Pagination";
+import Pagination from "../../Shared/Pagination";
 import {ref, watch} from "vue";
 import { Inertia } from "@inertiajs/inertia";
 
--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/', function () {
 });
 
 Route::get('/users', function (Request $request) {
-    return Inertia::render('Users', [
+    return Inertia::render('Users/Index', [
         'users' => User::query()
             ->when($request->input('search'), function ($query, $search) {
                 $query->where('name', 'like', "%{$search}%");
@@ -34,6 +34,22 @@ Route::get('/users', function (Request $request) {
             ]),
         'filters' => $request->only(['search']),
     ]);
+});
+
+Route::get('/users/create', function () {
+    return Inertia::render('Users/Create');
+});
+
+Route::post('/users', function (Request $request) {
+    $attributes = $request->validate([
+        'name' => 'required',
+        'email' => ['required', 'email'],
+        'password' => 'required',
+    ]);
+
+    User::create($attributes);
+
+    return redirect('/users');
 });
 
 Route::get('/settings', function () {
